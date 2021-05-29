072f2f555adcf4ef4dfc3542604de0ea64fb2d03
Mustaf321 <must.22@hotmail.de>
2021-05-29 21:07:57 +0200

    woow

--- App.js
+++ App.js
@@ -1,17 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput,Button} from 'react-native';
+import axios from "axios";
+
+
+
 
 export default function App() {
+
+  const [temperature, getTemperature] = React.useState();
+  const [config, setConfig]= React.useState({route: '' , wlan: '' , password:'' })
+
+
+
+  const sendConfig = () => {
+    let nam = {name}
+    let direction ={rout}
+    let secret = {password}
+    setConfig({ route: direction.rout, wlan: nam.name, password: secret.password});
+
+
+    console.log(config)
+    //axios.post()
+  }
+  let [rout, setRoute]= React.useState();
+  let [name, setName]= React.useState();
+  let [password, setPassword]= React.useState();
   return (
     <View style={styles.container}>
-      <View style={styles.header}><Text style={styles.fatText}>Header</Text></View>
+      <View style={styles.header}><Text style={styles.fatText}>Header </Text></View>
       <View style={styles.body}>
-        <TextInput style={styles.textField} placeholder="hello"></TextInput>
-        <TextInput style={styles.textField}></TextInput>
-        <TextInput style={styles.textField}></TextInput>
+        <TextInput
+            placeholder="http://exsampel"
+            onChangeText={(val)=> setRoute(val)}
+            style={styles.textField}></TextInput>
+        <TextInput
+            placeholder="Wlan name"
+            onChangeText={(val)=> setName(val)}
+            style={styles.textField}></TextInput>
+
+        <TextInput
+            placeholder="Password"
+            onChangeText={(val)=> setPassword(val)}
+            style={styles.textField}></TextInput>
+
         <View style={styles.subButton}>
-          <Button title="sub"></Button>
+          <Button
+              onPress={sendConfig}
+              title="sub"></Button>
         </View>
       </View>
       <StatusBar style="auto" />
