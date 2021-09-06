bbd7bf5440faf55e35610f6aff941a0b4a141b37
Mustaf321 <must.22@hotmail.de>
2021-09-06 22:28:50 +0200

    init structure

--- App.js
+++ App.js
@@ -1,88 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput,Button} from 'react-native';
-import axios from "axios";
+import React, {useState, useEffect} from 'react';
+import {TextInput} from 'react-native';
+import {View} from "react-native-web";
+import {Text} from "react-native-svg";
 
 
+    const App = () => {
+        const [measurement, getMeasurement] = useState( {temp: Math.random()/3 , Hum: Math.random()*2 , temp2: Math.random() , timestamp: Date.now()});
 
+        //ComponentDidUpdate
 
-export default function App() {
+        return (
+            <view>
+                <Text>Temp:</Text>
+                <Text>{measurement.temp}</Text>
+                <Text>{'\n'}</Text>
+                <Text>Hum:</Text>
+                <Text>{measurement.Hum}</Text>
+                <Text>Temp2:</Text>
+                <Text>{measurement.temp2}</Text>
+                <Text>date:</Text>
+                <Text>{measurement.timestamp}</Text>
+            </view>
 
-  const [temperature, getTemperature] = React.useState();
-  const [config, setConfig]= React.useState({route: '' , wlan: '' , password:'' })
+        );
+    };
 
-
-
-  const sendConfig = () => {
-    let nam = {name}
-    let direction ={rout}
-    let secret = {password}
-    setConfig({ route: direction.rout, wlan: nam.name, password: secret.password});
-
-
-    console.log(config)
-    //axios.post()
-  }
-  let [rout, setRoute]= React.useState();
-  let [name, setName]= React.useState();
-  let [password, setPassword]= React.useState();
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}><Text style={styles.fatText}>Header </Text></View>
-      <View style={styles.body}>
-        <TextInput
-            placeholder="http://exsampel"
-            onChangeText={(val)=> setRoute(val)}
-            style={styles.textField}></TextInput>
-        <TextInput
-            placeholder="Wlan name"
-            onChangeText={(val)=> setName(val)}
-            style={styles.textField}></TextInput>
-
-        <TextInput
-            placeholder="Password"
-            onChangeText={(val)=> setPassword(val)}
-            style={styles.textField}></TextInput>
-
-        <View style={styles.subButton}>
-          <Button
-              onPress={sendConfig}
-              title="sub"></Button>
-        </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    backgroundColor: '#0000ff',
-    padding : 20,
-  },
-
-  body: {
-    backgroundColor: '#ededed',
-    padding: 20,
-  },
-  textField:{
-    backgroundColor:'#bdbdbd',
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-  },
-  fatText:{
-    fontWeight: 'bold',
-  },
-  subButton:{
-    backgroundColor: '#4cbc00',
-    margin: 22,
-    height: 40,
-  }
-});
+    export default App;
---
+++ charts.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, Text, View ,Dimensions } from 'react-native';
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
+import App from "./App";
+export default function Chart() {
+
+    const chartConfig = {
+        backgroundGradientFrom: "#1E2923",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#08130D",
+        backgroundGradientToOpacity: 0.5,
+        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+    };
+  return (
+      <View>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+            data={{
+                labels: ["January", "February", "March", "April", "May", "June"],
+                datasets: [
+                        {
+                        data: [
+
+                            ]
+                    }
+                ]
+            }}
+            width={Dimensions.get("window").width} // from react-native
+            height={220}
+            yAxisLabel="$"
+            yAxisSuffix="k"
+            yLablesOffset={1}
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                    borderRadius: 16
+                },
+                propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                }
+            }}
+            bezier
+            style={{
+                marginVertical: 8,
+                borderRadius: 16
+            }}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default Chart;
